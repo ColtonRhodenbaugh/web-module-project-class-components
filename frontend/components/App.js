368ec42da7bb2c10f01ef46518319d9d44491ab7
Colton Rhodenbaugh <coltonrhodenbaugh@Coltons-Air.hsd1.il.comcast.net>
2023-03-02 19:29:42 -0600

    all done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './TodoList';
+import Form from './Form';
 
 
  class App extends React.Component {
@@ -20,21 +21,53 @@ import TodoList from './TodoList';
       ]
     }
   }
+
+  handleAdd = (name) => {
+
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false 
+    }
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+  handleToggle = (clickedId) => {
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo=>{
+        if(todo.id === clickedId) {
+          return{
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+          return todo;
+      })
+    });
+  }
+
   render() {
     const { todos } = this.state;
-    console.log(todos);
 
     return (
       <div>
        <h1>Todos</h1>
-        <TodoList todos={todos}/>
-
-       <form>
-        <input/>
-        <button>Add</button>
-       </form>
-
-       <button>Clear</button>
+        <TodoList handleToggle={this.handleToggle}todos={todos}/>
+        <Form handleAdd={this.handleAdd}/>
+       <button onClick={this.handleClear}>Clear</button>
       </div>
     );
   }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 class Todo extends React.Component {
+  handleClick = () => {
+    this.props.handleToggle();
+  }
   render() {
     return (
-      <li key={this.props.todo.id}>{this.props.todo.name} { this.props.todo.completed?<span>: Completed </span>:<span></span> }</li>
+      <li onClick={this.handleClick}>{this.props.todo.name} { this.props.todo.completed?<span>: Completed </span>:<span></span> }</li>
     )
   }
 }
